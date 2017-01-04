e7cc16adf3c6e4738ec21c9d9e1554dbc4681753
Tom Matheussen <tommatheussen5@hotmail.com>
2017-01-04 17:20:14 +0100

    Create dummy_secrets.sh

---
+++ bin/dummy_secrets.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "http_password: dummy" >> ../secrets.yaml
+echo "google_secret: dummy" >> ../secrets.yaml
+echo "pushbullet_api: dummy" >> ../secrets.yaml
+echo "lastfm_api: dummy" >> ../secrets.yaml
