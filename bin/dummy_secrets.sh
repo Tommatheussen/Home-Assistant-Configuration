0273ec09e39932c4dd3c6171e471fd7b699a5416
Tom Matheussen <tommatheussen5@hotmail.com>
2017-01-04 17:24:38 +0100

    Update dummy_secrets.sh

--- bin/dummy_secrets.sh
+++ bin/dummy_secrets.sh
@@ -4,3 +4,4 @@ echo "http_password: dummy" >> ../secrets.yaml
 echo "google_secret: dummy" >> ../secrets.yaml
 echo "pushbullet_api: dummy" >> ../secrets.yaml
 echo "lastfm_api: dummy" >> ../secrets.yaml
+echo "Did this actually work?"
