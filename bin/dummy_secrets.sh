0562ed4988bac650c5f8d5ab4a94efdfe609ffe4
Tom Matheussen <tommatheussen5@hotmail.com>
2017-01-09 16:55:55 +0100

    Added SSL support (#14)
    * Update configuration.yaml
    
    * Update dummy_secrets.sh
    
    * Update configuration.yaml
    
    * Update dummy_secrets.sh
    
    * Update dummy_secrets.sh

--- bin/dummy_secrets.sh
+++ bin/dummy_secrets.sh
@@ -8,3 +8,7 @@ echo "lastfm_user: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "owm_api: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "mqtt_pass: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "mqtt_username: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "ssl_key: ssl_key.pem" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "ssl_cert: ssl_cert.pem" >> $TRAVIS_BUILD_DIR/secrets.yaml
+touch "ssl_key.pem"
+touch "ssl_cert.pem"
