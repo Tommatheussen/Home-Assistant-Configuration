f78a437962ae4d5c59bd461e0f241a8cf417baaa
Tom Matheussen <tommatheussen5@hotmail.com>
2017-01-09 16:22:37 +0100

    Update dummy_secrets.sh

--- bin/dummy_secrets.sh
+++ bin/dummy_secrets.sh
@@ -8,5 +8,7 @@ echo "lastfm_user: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "owm_api: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "mqtt_pass: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "mqtt_username: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "ssl_key: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "ssl_cert: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "ssl_key: ssl/key.pem" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "ssl_cert: ssl/cert.pem" >> $TRAVIS_BUILD_DIR/secrets.yaml
+touch "ssl/key.pem"
+touch "ssl/cert.pem"
