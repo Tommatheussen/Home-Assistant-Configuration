26df3904dca1ff10da97ec36b5d5c073d10eb047
Tom Matheussen <tommatheussen5@hotmail.com>
2017-01-18 16:04:16 +0100

    Update dummy_secrets.sh

--- bin/dummy_secrets.sh
+++ bin/dummy_secrets.sh
@@ -10,5 +10,6 @@ echo "mqtt_pass: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "mqtt_username: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "ssl_key: ssl_key.pem" >> $TRAVIS_BUILD_DIR/secrets.yaml
 echo "ssl_cert: ssl_cert.pem" >> $TRAVIS_BUILD_DIR/secrets.yaml
+echo "db_url: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
 touch "ssl_key.pem"
 touch "ssl_cert.pem"
