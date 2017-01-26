927e01f76b602bde8d2d25eec1f16135a87ea389
Tom Matheussen <tommatheussen5@hotmail.com>
2017-01-26 13:03:56 +0100

    Fixed issues with bash script

--- bin/update_new_commits_sensor.sh
+++ bin/update_new_commits_sensor.sh
@@ -2,6 +2,6 @@
 
 cd "/home/hass/.homeassistant"
 git fetch
-commits = $(git rev-list --count master..origin/master)
+commits="$(git rev-list --count master..origin/master)"
 
-curl -X POST -H "x-ha-access: $1" -H "Content-Type: application/json" https://matheussen.serverpit.com:8123/api/states/sensor.new_commits -d '{"state": $commits}'
+curl -X POST -H "x-ha-access: $1" -H "Content-Type: application/json" https://matheussen.serverpit.com:8123/api/states/sensor.new_commits -d "{\"state\": $commits}"
