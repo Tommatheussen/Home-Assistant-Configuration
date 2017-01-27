15ebe71f5d7e6d0768139456ffc305bd4bc9e0e0
Tom Matheussen <tommatheussen5@hotmail.com>
2017-01-27 12:34:55 +0100

    Update update_devices_from_server.sh

--- bin/update_devices_from_server.sh
+++ bin/update_devices_from_server.sh
@@ -3,7 +3,8 @@
 cd "/home/hass/.homeassistant"
 git add known_devices.yaml
 git commit -m "New device from server"
+git push
 
-result="$(git push)"
-
-curl --verbose -X POST -H "x-ha-access: $1" -H "Content-Type: application/json" https://matheussen.serverpit.com:8123/api/events/device_failed -d "{\"reason\": \"$result\"}"
+if [ $? -ne 0]; then
+  curl --verbose -X POST -H "x-ha-access: $1" -H "Content-Type: application/json" https://matheussen.serverpit.com:8123/api/events/update_device_from_server_failed
+fi
