99f85be2da66e7f27e0f2b9244e4ae3d48e871ac
Tom Matheussen <tommatheussen5@hotmail.com>
2017-02-06 20:54:29 +0100

    Updated Schema and added all tasks as sensor

--- custom_components/sensor/flexget.py
+++ custom_components/sensor/flexget.py
@@ -1,5 +1,6 @@
 from homeassistant.helpers.entity import Entity
 from homeassistant.components.sensor import PLATFORM_SCHEMA
+import homeassistant.helpers.config_validation as cv
 from homeassistant.const import (CONF_NAME, CONF_PASSWORD, CONF_HOST)
 import logging
 import time
@@ -13,12 +14,8 @@ MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=15)
 _LOGGER = logging.getLogger(__name__)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_HOST): cv.string, default='http://localhost:5050/api',
-    vol.Required(CONF_API_KEY): cv.string,
-    vol.Optional(CONF_MONITORED_CONDITIONS, default=[]):
-        vol.All(cv.ensure_list, [vol.In(SENSOR_TYPES)]),
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_FORECAST, default=False): cv.boolean
+    vol.Optional(CONF_HOST, default='http://localhost:5050/api/'): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string
 })
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
@@ -26,34 +23,41 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     name = config.get(CONF_NAME)
     host = config.get(CONF_HOST)
     password = config.get(CONF_PASSWORD)
+
+    try:
+        r = requests.get(host + 'status/', auth=('flexget', password))
+    except requests.exceptions.RequestException:
+        _LOGGER.error("Failed to connect to the configured Flexget instance")
+        return False
+
+    if(r.status_code == 401):
+        _LOGGER.error("Authentication with Flexget failed")
+        return False
     
-    r = requests.get(host + '/status/', auth=('flexget', password))
-    print(r.json())
-        
-    #add_devices([FlexgetTaskSensor()])
+    add_devices([FlexgetTaskSensor(task) for task in r.json()])
 
-
-class ExampleSensor(Entity):
+class FlexgetTaskSensor(Entity):
     """Representation of a Sensor."""
-    def __init(self, name):
-    	self._state = None
-    	self._name = name
+    def __init__(self, task):
+        self._name = task['name']
+        self._last_execution = task['last_execution']
+        print(task)
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return 'Example Temperature'
+        return self._name
 
     @property
     def state(self):
         """Return the state of the sensor."""
-        return 23
+        return self._last_execution['succeeded']
 
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement."""
-        return TEMP_CELSIUS
+  #  @property
+  #  def unit_of_measurement(self):
+   #     """Return the unit of measurement."""
+    #    return TEMP_CELSIUS
         
-    @Throttle(MIN_TIME_BETWEEN_UPDATES)
-    def update(self):
-    	  """Get the latest data and updates the state and """
+    #@Throttle(MIN_TIME_BETWEEN_UPDATES)
+    #def update(self):
+    #	  """Get the latest data and updates the state and """
