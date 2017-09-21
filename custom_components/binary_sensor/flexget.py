491a538bb6a998956065fd8fba0d0dc5c4e2da10
Tom Matheussen <Tommatheussen5@hotmail.com>
2017-09-21 15:09:21 +0200

    Update Flexget sensor

--- custom_components/binary_sensor/flexget.py
+++ custom_components/binary_sensor/flexget.py
@@ -14,11 +14,13 @@ import requests
 MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=5)
 
 CONF_TASKS = 'tasks'
+CONF_SSL = 'ssl'
 
 _LOGGER = logging.getLogger(__name__)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_HOST, default='http://localhost:5050/api/'): cv.string,
+    vol.Optional(CONF_HOST, default='http://localhost:5050'): cv.string,
+    vol.Optional(CONF_SSL, default=True): cv.boolean,
     vol.Required(CONF_PASSWORD): cv.string,
     vol.Optional(CONF_TASKS, default=[]): cv.ensure_list
 })
@@ -28,9 +30,10 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     host = config.get(CONF_HOST)
     password = config.get(CONF_PASSWORD)
     tasks = config.get(CONF_TASKS)
+    ssl = config.get(CONF_SSL)
 
     try:
-        r = requests.get(host + 'status/', auth=('flexget', password), params=({'include_execution': False }))
+        r = requests.get(host + '/api/status/', auth=('flexget', password), params=({'include_execution': False }), verify=ssl)
     except requests.exceptions.RequestException:
         _LOGGER.error("Failed to connect to the configured Flexget instance: %e", requests.exceptions.RequestException)
         return False
@@ -38,17 +41,18 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     if(r.status_code == 401):
         _LOGGER.error("Authentication with Flexget failed")
         return False
-    
-    add_devices([FlexgetTaskSensor(task['name'], task['id'], host, password)
+
+    add_devices([FlexgetTaskSensor(task['name'], task['id'], host, password, ssl)
         for task in r.json() if (task['name'] in tasks or tasks == [])])
 
 class FlexgetTaskSensor(BinarySensorDevice):
     """Representation of a Sensor."""
-    def __init__(self, name, task_id, host, password):
+    def __init__(self, name, task_id, host, password, ssl):
         self._name = name
         self._id = task_id
         self._host = host
         self._password = password
+        self._ssl = ssl
         self.update()
 
     @property
@@ -70,5 +74,5 @@ class FlexgetTaskSensor(BinarySensorDevice):
     def update(self):
         """Get the latest data and updates the state and """
         # TODO: handle errors
-        r = requests.get(self._host + 'status/' + str(self._id) + '/', auth=('flexget', self._password))
+        r = requests.get(self._host + '/api/status/' + str(self._id) + '/', auth=('flexget', self._password), verify=self._ssl)
         self._last_execution = r.json()['last_execution']
