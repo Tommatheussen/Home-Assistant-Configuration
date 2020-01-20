41e936ac3a6bad3a71ee1401f3474c7552fd7101
Tom Matheussen <tommatheussen5@hotmail.com>
2020-01-20 11:48:43 +0100

    :sparkles: Added Home Connect

---
+++ custom_components/homeconnect/const.py
@@ -0,0 +1,6 @@
+"""Constants for the Home Connect integration."""
+
+DOMAIN = "homeconnect"
+
+OAUTH2_AUTHORIZE = "https://api.home-connect.com/security/oauth/authorize"
+OAUTH2_TOKEN = "https://api.home-connect.com/security/oauth/token"
