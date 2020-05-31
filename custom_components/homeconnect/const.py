c060a6049d55f699014d86249bc7d278c0c05265
root <root@a0d7b954-vscode.local.hass.io>
2020-05-31 14:24:04 +0200

    :boom: Move HomeConnect to official component

--- custom_components/homeconnect/const.py
+++
@@ -1,6 +0,0 @@
-"""Constants for the Home Connect integration."""
-
-DOMAIN = "homeconnect"
-
-OAUTH2_AUTHORIZE = "https://api.home-connect.com/security/oauth/authorize"
-OAUTH2_TOKEN = "https://api.home-connect.com/security/oauth/token"
