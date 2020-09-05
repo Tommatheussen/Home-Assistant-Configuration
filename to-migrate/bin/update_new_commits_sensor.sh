9e0275ef9ce29832cfb4153686c0b59a28f13a85
Tom Matheussen <tommatheussen5@hotmail.com>
2020-09-05 12:25:40 +0200

    :wastebasket: Removed old config updating stuff

--- to-migrate/bin/update_new_commits_sensor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "/home/hass/.homeassistant"
-git fetch
-commits="$(git rev-list --count master..origin/master)"
-
-curl -X POST -H "x-ha-access: $1" -H "Content-Type: application/json" https://matheussen.serverpit.com:8123/api/states/sensor.new_commits -d "{\"state\": \"$commits\"}"
