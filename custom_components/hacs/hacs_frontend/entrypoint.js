10cbeb0d656ff388c3db896907ec51815eca7c44
Tom Matheussen <tommatheussen5@hotmail.com>
2022-07-13 16:54:04 +0200

    :arrow_up: Update hacs

--- custom_components/hacs/hacs_frontend/entrypoint.js
+++ custom_components/hacs/hacs_frontend/entrypoint.js
@@ -1,9 +1,9 @@
 
 try {
-  new Function("import('/hacsfiles/frontend/main-f3e781b1.js')")();
+  new Function("import('/hacsfiles/frontend/main-a0d7432d.js')")();
 } catch (err) {
   var el = document.createElement('script');
-  el.src = '/hacsfiles/frontend/main-f3e781b1.js';
+  el.src = '/hacsfiles/frontend/main-a0d7432d.js';
   el.type = 'module';
   document.body.appendChild(el);
 }
